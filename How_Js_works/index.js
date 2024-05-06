db449f131ddf29217556bbfb30f5e91ab53b5119
manish-dev3490 <kmanish87064@gmail.com>
2024-05-06 19:45:14 +0530

    done

---
+++ How_Js_works/index.js
@@ -0,0 +1,9 @@
+// Lets look into the call stack , excecution context and how js works behind the scenes
+// var a=10;
+// function square(num){
+//     var answer=num*num;
+//     return answer;
+// } 
+// var squareOne=square(2);
+// var squaretwo=square(a);
+
