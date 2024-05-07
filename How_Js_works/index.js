2bd842f7d3a3aa786c5dd61142e8a3e80d09a765
manish-dev3490 <kmanish87064@gmail.com>
2024-05-07 16:10:14 +0530

    shadowing with let and cosnnt

--- How_Js_works/index.js
+++ How_Js_works/index.js
@@ -76,7 +76,22 @@
 // }
 
 
-// Shadowing in Javascript
+// Shadowing in Javascript with var
 // var a=10;
 // var a=undefined;
 // console.log(a);
+
+// Shadowing in Javascript with let
+let a=10
+{
+    let a=20;
+    var b=30;
+    const c=40;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+
+}
+
+console.log(a);
