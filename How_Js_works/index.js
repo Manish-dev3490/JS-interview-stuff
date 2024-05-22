af16268d0c728b066b018aa9308b2de7fb85cedf
manish-dev3490 <kmanish87064@gmail.com>
2024-05-22 13:00:19 +0530

    promise.all implemented

--- How_Js_works/index.js
+++ How_Js_works/index.js
@@ -295,3 +295,33 @@
 // }).catch(()=>{
 //     console.log("you have error in make payment");
 // })
+
+
+
+// promise Apis promise.all/settled/race/any
+
+// const p1 = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         resolve("p1 reject")
+
+//     }, 5000)
+// })
+
+// const p2 = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         reject("p2 reject")
+
+//     }, 1000)
+// })
+
+// const p3 = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         resolve("p3 resolved")
+
+//     }, 3000)
+// })
+
+
+// Promise.all([p1, p2, p3]).then((response) => {
+//     console.log(response);
+// }).catch((err)=>console.log(err));
